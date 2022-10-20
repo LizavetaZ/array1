ce9f5d1c75687589380b1603851bedba007d04b9
Лиза за <lizuska@yandex.ru>
2022-10-20 19:05:14 +0300

    CountDigit >0

---
+++ Case041/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите элементы массива через запятую");
+int[] Numbers = ArrayFromString(Console.ReadLine());
+int count = InputCount(Numbers, 0);
+Console.WriteLine("Количество элементов больше 0 - " + count);
+
+
+int[] ArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(",", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+int InputCount(int[] array, int countDigit)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            countDigit = countDigit + 1;
+        }
+    }
+    return countDigit;
+}
+
